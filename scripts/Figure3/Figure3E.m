30367a914385b7dea773d653f3d5ea227f408875
Barra <barrab01@VEERAP01-14MBP.lan>
2022-10-28 00:13:59 -0400

    Added Figure 3B and 3E

---
+++ scripts/Figure3/Figure3E.m
@@ -0,0 +1,62 @@
+%%
+% @Author: barrab
+% @Date:   2019-06-13 12:14:23
+
+%% Loading dataset 
+
+
+% Load data
+datapath = 'datapath'; 
+Elbow=  readtable(fullfile(datapath, 'Figure3E_ElbowExtension.csv')); 
+Wrist=  readtable(fullfile(datapath, 'Figure3E_WristFlexion.csv')); 
+
+
+% Load colors
+OneTrueCM; 
+
+
+%% Plot Figure 3B
+
+figure
+frequencies = Elbow.Properties.VariableNames'; 
+
+nFreq = length(frequencies); 
+subplot(1, 2, 1)
+hold on
+for iF = 2 : nFreq
+    datapoints = Elbow.(frequencies{iF}); 
+    
+    plot(iF*ones(size(datapoints))+ 0.2*(rand(length(datapoints),1)-0.5), datapoints, 'o',...
+        'MarkerFaceColor', [0.5, 0.5, 0.5], ...
+        'MarkerEdgeColor', [0, 0, 0])
+    plot(iF, nanmedian(datapoints), 'o',...
+        'MarkerFaceColor', [0, 0, 0], ...
+        'MarkerEdgeColor', [0, 0, 0], ...
+        'MarkerSize', 12)
+    
+end
+
+ylim([0 60])
+ylabel('degrees')
+set(gca, 'xtick', [1 : length(frequencies)], 'xticklabel', frequencies)
+title('Elbow excursion')
+
+subplot(1, 2, 2)
+frequencies = Wrist.Properties.VariableNames'; 
+hold on
+for iF = 2 : nFreq
+    datapoints = Wrist.(frequencies{iF}); 
+    
+    plot(iF*ones(size(datapoints))+ 0.2*(rand(length(datapoints),1)-0.5), datapoints, 'o',...
+        'MarkerFaceColor', [0.5, 0.5, 0.5], ...
+        'MarkerEdgeColor', [0, 0, 0])
+     plot(iF, nanmedian(datapoints), 'o',...
+        'MarkerFaceColor', [0, 0, 0], ...
+        'MarkerEdgeColor', [0, 0, 0], ...
+        'MarkerSize', 12)
+    
+end
+
+ylabel('degrees')
+set(gca, 'xtick', [1 : length(frequencies)], 'xticklabel', frequencies)
+title('Wrist Flexion')
